8b1a034fcd0809466737955bb9cc20fd45408fca
varun <varunkgangala123@gmail.com>
2026-06-20 22:14:00 +0530

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1)>len(s2):
+            return False
+        array1=list(s1)
+        array2=list(s2)
+        res=[]
+        target=sorted(array1)
+        for i in range(len(array1)):
+            res.append(array2[i])
+        if sorted(res)==target:
+            return True
+        left=0
+        for right in range(len(array1),len(array2)):
+            res.pop(0)
+            res.append(array2[right])
+            if sorted(res)==target:
+                return True
+        return False
